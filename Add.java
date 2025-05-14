87be9bdc3cc76373e9620fcebad75d0411ab34e8
Milind Yadav <milindyadav2310@gmail.com>
2025-05-14 18:12:24 +0530

    added message in

--- Add.java
+++ Add.java
@@ -2,6 +2,6 @@ public class Add {
     public static void main(String[] args) {
         int a = 5, b = 10;
         int sum = a + b;
-        System.out.println("Sum: " + sum);
+        System.out.println("Sum is : " + sum);
     }
 }
